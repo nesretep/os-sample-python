e40bf918e1978cb3f95ae068bd652ce9894d2abd
Kristian Petersen <nesretep@cs.byu.edu>
2018-10-19 09:16:37 -0600

    running test

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,18 @@
+# import requests as url
+import logging
 from flask import Flask, request
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    param = request.args['test']
-    return f"Hello {param}!"
+@application.route("/labels")
+def print():
+    try:
+        username = request.args['username']
+        password = request.args['password']
+        print_data = request.args['printData']
+    except Exception as post_error:
+        pass
+
+    return f"username: {username}; password: {password}; printData: {print_data}"
 
 if __name__ == "__main__":
     application.run(debug=True)
