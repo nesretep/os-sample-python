bb98eed1021d08d2bc297808951284f3de400e76
Kristian Petersen <nesretep.naitsirk@gmail.com>
2019-09-30 13:13:20 -0600

    more testing

--- wsgi.py
+++ wsgi.py
@@ -47,9 +47,9 @@ def printer(ipaddress='192.168.101.35', port=9100, test=None):
         # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # client_socket.settimeout(20)
         client_socket.connect((ipaddress, port))
-        # bytes_sent = client_socket.send(print_data)
-        # return f"{bytes_sent} bytes were written successfully."
-        return "done"
+        bytes_sent = client_socket.send(print_data)
+        return f"{bytes_sent} bytes were written successfully."
+        # return "done"
     except socket.error as socket_error:
         return f"Socket Error: {socket_error}"
     except Exception as post_error:
